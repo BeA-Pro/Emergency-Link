25b7100e1c572417a2de7957968e1b403c8df46e
YongHyunKing <yhkim8246@gmail.com>
2024-07-19 00:11:10 +0900

    feat: useInput 커스텀 훅 생성

---
+++ el-front/src/hooks/customhooks.ts
@@ -0,0 +1,14 @@
+import {useState, ChangeEvent} from 'react'
+
+const useInput = (initialValue : string) => {
+  const [inputValue, setInputValue] = useState(initialValue);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) =>{
+    setInputValue(e.target.value);
+    console.log(inputValue);
+  }
+
+  return [inputValue, handleChange] as const;
+}
+
+export {useInput}
