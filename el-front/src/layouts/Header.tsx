028e6ea0ebd482d20a34e5935904a1c8a91d3470
YongHyunKing <yhkim8246@gmail.com>
2024-07-18 15:45:09 +0900

    feat: 헤더 푸터 구현

--- el-front/src/layouts/Header.tsx
+++ el-front/src/layouts/Header.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+
+// scss
 import styles from '@stylesLayouts/Headers.module.scss';
 
+// assets
 import logo from '@logos/horizontal-logo.svg';
+import logIn from '@icons/login-icons.svg';
+
 const Header = () => {
+  
   return (
     <header className={styles.wrapper}>
-      <img src={logo} alt={logo} className={styles.logo}/>
+      <img src={logo} alt="logo" className={styles.logo}/>
+      <img src={logIn} alt="logIn" className={styles.logIn}/>
     </header>
   )
 }
