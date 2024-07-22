7c70afe68c41700a3803b53dd68bfd6d872d1239
YongHyunKing <yhkim8246@gmail.com>
2024-07-22 10:36:15 +0900

    feat: spring-boot backend 설치

---
+++ el-back/src/main/java/com/emergency_link/emergency_link/EmergencyLinkApplication.java
@@ -0,0 +1,13 @@
+package com.emergency_link.emergency_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyLinkApplication.class, args);
+	}
+
+}
---
+++ el-back/src/test/java/com/emergency_link/emergency_link/EmergencyLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emergency_link.emergency_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
