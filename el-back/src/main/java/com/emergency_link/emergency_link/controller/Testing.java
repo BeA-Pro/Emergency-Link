59e19c505be0ae5762c91e984cdaa06536d3bfd2
YongHyunKing <yhkim8246@gmail.com>
2024-08-05 23:38:50 +0900

    feat: 테스트용 컨트롤러 추가

---
+++ el-back/src/main/java/com/emergency_link/emergency_link/controller/Testing.java
@@ -0,0 +1,13 @@
+package com.emergency_link.emergency_link.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Testing {
+    @GetMapping("/testing")
+    public String testing(){
+        return "Hello!";
+    }
+}
