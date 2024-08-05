a7b55c638dad4b0fac39fdf281168206beec5941
YongHyunKing <yhkim8246@gmail.com>
2024-08-06 00:54:05 +0900

    test: cd 테스트

--- el-back/src/main/java/com/emergency_link/emergency_link/controller/Testing.java
+++ el-back/src/main/java/com/emergency_link/emergency_link/controller/Testing.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Testing {
     @GetMapping("/testing")
     public String testing(){
-        return "Hello!!!";
+        return "Hello!!!!";
     }
 }
