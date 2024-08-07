4dd89cfab96eeb91f7f712372cbb38fe90128662
YongHyunKing <yhkim8246@gmail.com>
2024-08-07 17:18:06 +0900

    test

--- el-back/src/main/java/com/emergency_link/emergency_link/controller/Testing.java
+++ el-back/src/main/java/com/emergency_link/emergency_link/controller/Testing.java
@@ -1,13 +1,43 @@
 package com.emergency_link.emergency_link.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class Testing {
+
+    @Value("${server.env}")
+    private String env;
+    @Value("${server.port}")
+    private String serverPort;
+    @Value("${server.serverAddress}")
+    private String serverAddress;
+    @Value("${serverName}")
+    private String serverName;
+
     @GetMapping("/testing")
     public String testing(){
         return "I love you!";
+    }
+
+    @GetMapping("/hc")
+    public ResponseEntity<?> healthCheck(){
+        Map<String, String> responseData = new HashMap<>();
+        responseData.put("serverName",serverName);
+        responseData.put("serverAddress",serverAddress);
+        responseData.put("serverPort",serverPort);
+        responseData.put("env",env);
+        return ResponseEntity.ok(responseData);
+    }
+
+    @GetMapping("env")
+    public ResponseEntity<?> getEnv(){
+        return ResponseEntity.ok(env);
     }
 }
