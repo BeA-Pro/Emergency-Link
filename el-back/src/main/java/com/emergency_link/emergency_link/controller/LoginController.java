9be5e513f3b49cc77571f029c10fc587da4b46e1
윤영인 <yun-yeong-in@yun-yeong-in-ui-MacBookAir.local>
2024-08-24 01:05:38 +0900

    feat: 스프링 시큐리티와 jwt를 활용한 로그인 구현

--- el-back/src/main/java/com/emergency_link/emergency_link/controller/LoginController.java
+++ el-back/src/main/java/com/emergency_link/emergency_link/controller/LoginController.java
@@ -1,51 +1,40 @@
 package com.emergency_link.emergency_link.controller;
 
-import com.emergency_link.emergency_link.dto.LoginData;
-import com.emergency_link.emergency_link.entity.UserInfo;
-import com.emergency_link.emergency_link.repository.UserInfoRepository;
-import com.emergency_link.emergency_link.service.JwtUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class LoginController {
 
-    private final UserInfoRepository userInfoRepository;
-    private final JwtUtil jwtUtil;
-
-    @Autowired
-    public LoginController(UserInfoRepository userInfoRepository, JwtUtil jwtUtil) {
-        this.userInfoRepository = userInfoRepository;
-        this.jwtUtil = jwtUtil;
-    }
-
-
-    @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> login(@RequestBody LoginData loginData){
-        System.out.println(loginData.getUserId()+" "+loginData.getPwd());
-        Optional<UserInfo> userInfo = userInfoRepository.findByUserIdAndUserPwd(loginData.getUserId(), loginData.getPwd());
-        Map<String, String> responseBody = new HashMap<>();
-        if(userInfo.isPresent()){
-            System.out.println("exist");
-            String token = jwtUtil.generateToken(userInfo.get().getUserId(), 1);
-
-            responseBody.put("status", "success");
-            responseBody.put("token", token);
-
-            return ResponseEntity.ok().body(responseBody);
-        }
-        System.out.println("Not exist");
-
-        responseBody.put("status", "fail");
-        responseBody.put("message", "Login fail");
-
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
-    }
+//    private final UserInfoRepository userInfoRepository;
+//    private final JwtUtil jwtUtil;
+//
+//    @Autowired
+//    public LoginController(UserInfoRepository userInfoRepository, JwtUtil jwtUtil) {
+//        this.userInfoRepository = userInfoRepository;
+//        this.jwtUtil = jwtUtil;
+//    }
+//
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<Map<String, String>> login(@RequestBody LoginData loginData){
+//        System.out.println(loginData.getUserId()+" "+loginData.getPwd());
+//        Optional<UserInfo> userInfo = userInfoRepository.findByUserIdAndUserPwd(loginData.getUserId(), loginData.getPwd());
+//        Map<String, String> responseBody = new HashMap<>();
+//        if(userInfo.isPresent()){
+//            System.out.println("exist");
+//            String token = jwtUtil.generateToken(userInfo.get().getUserId(), 1);
+//
+//            responseBody.put("status", "success");
+//            responseBody.put("token", token);
+//
+//            return ResponseEntity.ok().body(responseBody);
+//        }
+//        System.out.println("Not exist");
+//
+//        responseBody.put("status", "fail");
+//        responseBody.put("message", "Login fail");
+//
+//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(responseBody);
+//    }
 }
---
+++ el-back/src/main/java/com/emergency_link/emergency_link/security/jwt/JwtRequestFilter.java
@@ -0,0 +1,87 @@
+package com.emergency_link.emergency_link.security.jwt;
+
+import com.emergency_link.emergency_link.entity.UserInfo;
+import com.emergency_link.emergency_link.security.auth.CustomUserDetails;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+import java.io.IOException;
+
+public class JwtRequestFilter extends OncePerRequestFilter {
+    private final JwtUtil jwtUtil;
+
+    public JwtRequestFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+//        // 로그인 경로는 필터를 통과하지 않도록 설정
+//        if (request.getServletPath().equals("/api/login")) {
+//            chain.doFilter(request, response);
+//            return;
+//        }
+
+        final String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            System.out.println("token null");
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String token = authorizationHeader.substring(7);
+
+        if (jwtUtil.isTokenExpired(token)) { // 토큰 만료
+            System.out.println("token expired");
+
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+            response.setContentType("application/json");
+            response.getWriter().write("{\"message\": \"Expired JWT token\"}");
+
+            //조건이 해당되면 메소드 종료 (필수)
+            return;
+
+        }
+
+        String userId = jwtUtil.extractUsername(token);
+        String role = jwtUtil.extractRole(token);
+        String password = jwtUtil.extractPassword(token);
+
+        if (!jwtUtil.validateToken(token, userId)) { // 유효하지 않은 토큰
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+            response.setContentType("application/json");
+            response.getWriter().write("{\"message\": \"Invalid JWT token\"}");
+            return;
+        }
+
+
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserId(userId);
+        userInfo.setUserPwd(password);
+        userInfo.setRole(role);
+
+        CustomUserDetails customUserDetails = new CustomUserDetails(userInfo);
+
+        // 스프링 시큐리티 인증 토큰 생성
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+
+        // 세션에 사용자 등록
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        chain.doFilter(request, response); // 다음 필터로 요청 전달
+
+    }
+}
